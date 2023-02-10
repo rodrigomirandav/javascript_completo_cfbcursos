ac4fb90c27b65558b8d424f34095dc9ede76bf78
Rodrigo de Miranda Videira <diguimirandajf@gmail.com>
2023-02-10 00:34:04 -0300

    Aula 09

---
+++ aula09.js
@@ -0,0 +1,12 @@
+/*
+Pré e o pós incremento ou decremento
+ */
+
+
+let num1 = 10
+
+console.log(num1++) // Pós -> 10
+console.log(++num1) // Pré -> 12
+
+console.log(num1--) // Pós -> 12
+console.log(--num1) // Pré -> 10
