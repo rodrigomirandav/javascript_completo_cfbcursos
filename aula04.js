7674ae0decac2374937abf63971faf84ff42ff4d
Rodrigo de Miranda Videira <diguimirandajf@gmail.com>
2023-02-09 23:43:13 -0300

    aula 04

---
+++ aula04.js
@@ -0,0 +1,19 @@
+/* 
+Operadores aritimeticos
+*/
+
+// + - * / % **  ++ --
+
+let num1 = 15, num2 = 2
+
+console.log(num1)
+console.log(num2)
+
+// % -> resto
+console.log(num2 % num1)
+
+// ** -> radical
+console.log(num1 ** num2)
+
+
+
