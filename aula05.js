a51305128195af1dcd8d02934a814a211689eb0c
Rodrigo de Miranda Videira <diguimirandajf@gmail.com>
2023-02-09 23:49:48 -0300

    aula 05

---
+++ aula05.js
@@ -0,0 +1,14 @@
+/*
+Operadores relacionais
+*/
+
+// > >= < <= == !=
+
+// Atenção
+
+let num1 = 10
+let num2 = 50
+
+console.log(num1 == num2)  // Igualdade, == pois o = somente é de atribuição
+
+console.log(num1 != num2)  // Desigualdade, utiliza o ! que é um operador de negação
